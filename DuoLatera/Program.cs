6b1b8af4a61225e2618541e1cf96613e6b5ff775
delyanb <100796212+delyanb@users.noreply.github.com>
2024-12-10 16:04:32 +0200

    fixed db connection

--- DuoLatera/Program.cs
+++ DuoLatera/Program.cs
@@ -7,18 +7,19 @@ using DuoLatera.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>
-    (options => options.UseSqlServer
-    (builder.Configuration.GetConnectionString
-    ("DefaultConnection")));
+//set up the database here
+//builder.Services.AddDbContext<ApplicationDbContext>
+    //(options => options.UseSqlServer
+    //(builder.Configuration.GetConnectionString
+    //("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
